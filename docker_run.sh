5001dce5b7341701342d530d90ceb91b4e43f48f
Ronan Delacroix <ronan.delacroix@gmail.com>
2018-03-28 18:29:22 +0200

    Fix

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
-docker build -t ronhanson/jobmanager-builder .
+#docker build -t ronhanson/jobmanager-builder .
 
-docker run --rm -ti --name jobmanager-builder -p 5001:5001 -v /private/var/run/docker.sock:/private/var/run/docker.sock $*
+docker run --rm -ti --name jobmanager-builder -p 5001:5001 -v /private/var/run/docker.sock:/private/var/run/docker.sock ronhanson/jobmanager-builder $* 
